d60f5d2176067fb7ad914c0ea88de4791cc0a278
快递100API <68226294+kuaidi100-api@users.noreply.github.com>
2025-05-09 13:33:21 +0800

    Update account.go

--- config/account.go
+++ config/account.go
@@ -1,8 +1,8 @@
 package config
 
 const (
-	KEY      = "hFBwtSji7088" // 客户授权key
+	KEY      = "hFBwtSba2088" // 客户授权key
 	SECRET   = "***"          // 秘钥
-	CUSTOMER = "89F42AFC3276B70D2F517B7AE23925E2"
-	USER_ID  = "9974ef2c377a4dbt9"
+	CUSTOMER = "89F42AFC1236B70D2F517B7AE22225E2"
+	USER_ID  = "1234ef2c377a4dbt9"
 )
